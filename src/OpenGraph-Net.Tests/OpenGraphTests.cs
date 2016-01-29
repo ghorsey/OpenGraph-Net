43c99303753e86d3e0f7834d3ad5163f92a5e639
Geoff Horsey <geoff.horsey@outlook.com>
2016-01-28 19:05:50 -0800

    updated unit tests

--- src/OpenGraph-Net.Tests/OpenGraphTests.cs
+++ src/OpenGraph-Net.Tests/OpenGraphTests.cs
@@ -178,9 +178,9 @@ namespace OpenGraph_Net.Tests
             OpenGraph graph = OpenGraph.ParseUrl("http://www.amazon.com/Spaced-Complete-Simon-Pegg/dp/B0019MFY3Q");
 
             Assert.AreEqual("http://www.amazon.com/dp/B0019MFY3Q/ref=tsm_1_fb_lk", graph.Url.ToString());
-            Assert.AreEqual("Spaced: The Complete Series", graph.Title);
+            Assert.IsTrue(graph.Title.StartsWith("Spaced: The Complete Series"));
             Assert.IsTrue(graph["description"].Contains("Spaced"));
-            Assert.AreEqual("http://ecx.images-amazon.com/images/I/51sWMo4CBcL._SL160_.jpg", graph.Image.ToString());
+            Assert.IsTrue(graph.Image.ToString().StartsWith("http://ecx.images-amazon.com/images/I"));
             Assert.AreEqual("movie", graph.Type);
             Assert.AreEqual("Amazon.com", graph["site_name"]);
         }
